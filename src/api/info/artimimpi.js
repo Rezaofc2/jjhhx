344d0395bb22523f75b8082b5f643f8f7da3d037
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2025-03-27 23:24:07 +0700

    ngantuk bg

--- src/api/info/artimimpi.js
+++ src/api/info/artimimpi.js
@@ -4,34 +4,34 @@ const cheerio = require("cheerio"); // Pastikan untuk mengimpor cheerio
 module.exports = (app) => {
   async function tafsir_mimpi(value) {
     return new Promise((resolve, reject) => {
-        axios.get('https://primbon.com/tafsir_mimpi.php?mimpi='+value+'&submit=+Submit+')
+      axios.get('https://primbon.com/tafsir_mimpi.php?mimpi=' + value + '&submit=+Submit+')
         .then(({ data }) => {
-            let $ = cheerio.load(data)
-            let fetchText = $('#body').text()
-            let hasil
-            try {
-                hasil = {
-              
-                        mimpi: value,
-                        arti: fetchText.split(`Hasil pencarian untuk kata kunci: ${value}`)[1].split('\n')[0],
-                        solusi: fetchText.split('Solusi -')[1].trim()
-                    
-                }
-            } catch {
-                hasil = {
-                   
-                    message: `Tidak ditemukan tafsir mimpi "${value}" Cari dengan kata kunci yang lain.`
-                }
-            }
-            resolve(hasil)
+          let $ = cheerio.load(data);
+          let fetchText = $('#body').text();
+          let hasil;
+          try {
+            hasil = {
+              mimpi: value,
+              arti: fetchText.split(`Hasil pencarian untuk kata kunci: ${value}`)[1].split('\n')[0],
+              solusi: fetchText.split('Solusi -')[1].trim()
+            };
+          } catch (error) {
+            hasil = {
+              message: `Tidak ditemukan tafsir mimpi "${value}". Cari dengan kata kunci yang lain.`
+            };
+          }
+          resolve(hasil);
         })
-    })
-}
+        .catch(error => {
+          reject(error); // Menangani kesalahan dari axios
+        });
+    });
+  }
 
   app.get("/info/artimimpi", async (req, res) => {
     try {
       const { text } = req.query;
-      if (!nama) {
+      if (!text) { // Ganti 'nama' dengan 'text'
         return res.status(400).json({ status: false, error: "Query is required" });
       }
       const result = await tafsir_mimpi(text);
