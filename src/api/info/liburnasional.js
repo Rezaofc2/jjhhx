8359058c920df6f85800f157e31700acc427f141
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2025-03-28 18:34:03 +0700

    Create liburnasional.js

---
+++ src/api/info/liburnasional.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+
+module.exports = (app) => {
+  async function liburnasional() {
+    try {
+      const response = await axios.get("https://api.siputzx.my.id/api/info/liburnasional");
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching content:", error);
+      throw error;
+    }
+  }
+
+  app.get("/info/liburnasional", async (req, res) => {
+    try {
+      const result = await liburnasional();
+      res.status(200).json({
+        status: true,
+        result,
+      });
+    } catch (error) {
+      res.status(500).json({ status: false, error: error.message });
+    }
+  });
+};
