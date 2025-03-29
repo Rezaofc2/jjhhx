2582232b6069d2961bec22d54ddcc48b6036f077
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2025-03-29 16:38:03 +0700

    Create videy.js

---
+++ src/api/downloader/videy.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+
+module.exports = (app) => {
+  async function videy(url) {
+    try {
+      const response = await axios.get(`https://api.diioffc.web.id/api/download/videy?url=${encodeURIComponent(url)}`);
+      return response.data.hasil;
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+      throw new Error("Unable to fetch weather data");
+    }
+  }
+
+  app.get("/downloader/videy", async (req, res) => {
+    const { url } = req.query;
+    
+    if (!url) {
+      return res.status(400).json({ status: false, error: "Query is required" });
+    }
+
+    try {
+      const result = await videy(q);
+      res.status(200).json({
+        status: true,
+        result,
+      });
+    } catch (error) {
+      res.status(500).json({ status: false, error: error.message });
+    }
+  });
+};
