97ff5fab53ebb0e5c3ce6a87e82932bdae873094
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2025-04-21 17:06:03 +0700

    Create bkp.js

---
+++ src/api/nsfw/bkp.js
@@ -0,0 +1,112 @@
+let fetch = require('node-fetch')
+module.exports = (app) => {
+  app.get("/nsfw/bkp", async (req, res) => {
+    try {
+      const viral = [
+
+'https://www.mediafire.com/file/8jr96kmpe4f6osz/1714577016765.mp4/file', 
+"https://www.mediafire.com/file/5aimbha2fn86e1j/vid_81/file?dkey=h7dm688by5e&r=1598",
+"https://www.mediafire.com/file/d6x8tevtqghvnis/Seks%20Anak%20Muda%20Nihh%20mp4.mp4/file",
+"https://www.mediafire.com/file/qxaz5va2n7zcjh0/Onlyfans_SAIZNEKO_jadi_pelayan_cafe_sambil_pamer_memek.mp4/file",
+"https://www.mediafire.com/file/b8y94yvcxqecpap/lesby.mp4/file",
+"https://www.mediafire.com/file/7pcbmfzug0jvkrq/Brut_1.mp4/file",
+"https://www.mediafire.com/file/q88tdotpejim67s/AYANG_SUKA_ISEP.mp4/file",
+"https://www.mediafire.com/file/a966474dwktwb8k/CRT_DI_LUAR.mp4/file",
+"https://www.mediafire.com/file/65wexarrbl0tzwk/DI_GESEK_SAMA_ADEK.mp4/file",
+"https://www.mediafire.com/file/ekqslifa6wv1rkk/ENAK_DI_ATAS.mp4/file",
+"https://www.mediafire.com/file/r3uc1eg0cweptux/INDO_FULL_PERCAKAPAN.mp4/file",
+"https://www.mediafire.com/file/nl870fz8r2pj82f/JILBAB_PASRAH.mp4/file",
+"https://www.mediafire.com/file/n60xl5yeu1848jr/TEMBEM.mp4/file",
+"https://www.mediafire.com/file/sln57yivq590gec/MAIN_SAMA_YANG_DI_GUBUG.mp4/file",
+"https://www.mediafire.com/file/uxe8mdztfb2c3st/SMP_MAIN_SAMA_OM_OM.mp4/file",
+"https://www.mediafire.com/file/dnq8f8uctddqjds/SAMBIL_NONTON.mp4/file",
+"https://www.mediafire.com/file/ou9udl9x44bc42r/RARA_NADIFA.mp4/file",
+"https://www.mediafire.com/file/0szutn32k6njtxy/PAP_KE_AYANG.mp4/file",
+"https://www.mediafire.com/file/ze1e0pwods8qmni/PAP_KE_AYANG_..mp4/file",
+"https://www.mediafire.com/file/9fjpmvlfiwygaii/MASIH_SEGEL.mp4/file",
+"https://www.mediafire.com/file/vr6peulv21ofvih/OCIL_SMP_VIR4L.mp4/file",
+"https://www.mediafire.com/file/sln57yivq590gec/MAIN_SAMA_YANG_DI_GUBUG.mp4/file",
+"https://www.mediafire.com/file/inzjbrfyxlh5j96/Brut_2.mp4/file",
+"https://www.mediafire.com/file/piwsi434sybebqw/Gabisa_masuk.mp4/file?dkey=dpd3ijfue8v&r=1968",
+"https://www.mediafire.com/file/ve8fpizx08b2o2u/bpkk%20koss.mp4/file",
+"https://www.mediafire.com/file/js38ooqc5fex0g8/Ganggu+Kakak+Tali+Ungu.mp4/file",
+"https://www.mediafire.com/file/bl31z1668mr9aea/Chindo+Depan+Cermin.mp4/file",
+"https://www.mediafire.com/file/emw2mcj9b4j8f8e/1s4p1n%20p2ny4%204d1k.mp4/file",
+"https://www.mediafire.com/file/kexkkzq3llxwsl7/Bd_a28_2.mp4/file?dkey=04eny7ovh3z&r=1546",
+"https://www.mediafire.com/file/2sc0ktgtvs1zvj2/Bd_a28_1.mp4/file",
+"https://www.mediafire.com/file/lhkxnokbdokb970/Ucill5/file?dkey=qzsmv5jnnto&r=370",
+"https://www.mediafire.com/file/3s42583vdg8q9rz/Ucil4/file",
+"https://www.mediafire.com/file/u3vij0vgmi8njhw/Ucil7/file",
+"https://www.mediafire.com/file/vl2o4rw8yl9vq60/Ucil8/file",
+"https://www.mediafire.com/file/lhkxnokbdokb970/Ucill5/file",
+"https://www.mediafire.com/file/ghxuibm62bmw5lv/adek_kakak.mp4/file?dkey=5qhvrcgd8bm&r=610",
+"https://www.mediafire.com/file/jiebzdr7jxk7mb6/5_6111746562516323380.mp4/file?dkey=vabsi35ejx7&r=952",
+"https://www.mediafire.com/file/wb3tq5yv5mnnuih/15.mp4/file",
+"https://www.mediafire.com/file/cwsi7wgb8iv8kfe/14.mp4/file",
+"https://www.mediafire.com/file/i116m8h10ntfqsv/13.mp4/file",
+"https://www.mediafire.com/file/t75knvu4wjia52s/12.mp4/file",
+"https://www.mediafire.com/file/ti6w9dslxyqz5na/11.mp4/file",
+"https://www.mediafire.com/file/fwtjvbt2t0y2kh4/video_%25283%2529.mp4/file",
+"https://www.mediafire.com/file/sbft6bgoxohwzdb/video_%25282%2529.mp4/file",
+"https://www.mediafire.com/file/sbft6bgoxohwzdb/video_%25282%2529.mp4/file",
+"https://www.mediafire.com/file/eedxe4054kc26i3/video_%25280%2529.mp4/file",
+"https://www.mediafire.com/file/jw4v9btrwsqbp7b/video.mp4/file",
+"https://www.mediafire.com/file/awpn8zdqad59xtz/10.mp4/file",
+"https://www.mediafire.com/file/d7fpgeqw97mixfw/9.mp4/file",
+"https://www.mediafire.com/file/r6hyo5gz8ooueeq/8.mp4/file",
+"https://www.mediafire.com/file/titdsbjehkuxhpw/7.mp4/file",
+"https://www.mediafire.com/file/7vdg85rqrlxfhga/6.mp4/file",
+"https://www.mediafire.com/file/60407gwpk429jno/5.mp4/file",
+"https://www.mediafire.com/file/2s3q66kc8b8fyfg/3.mp4/file",
+"https://www.mediafire.com/file/ybeng2hl8w4brf1/ojol1.mp4/file",
+"https://www.mediafire.com/file/sp4nirjdw1tdtpw/ojol2.mp4/file",
+"https://www.mediafire.com/file/jz3jo6m941ty2yv/ojol3.mp4/file",
+"https://www.mediafire.com/file/jz3jo6m941ty2yv/ojol3.mp4/file",
+"https://www.mediafire.com/file/237qjanqu5pdozj/ojol5.mp4/file",
+"https://www.mediafire.com/file/n29573bspbfqa1o/ojol6.mp4/file",
+"https://www.mediafire.com/file/qf9n3l2umu65ppg/mabar_d1k_cant1k.mp4/file?dkey=8su8a2a2rwt&r=216",
+"https://www.mediafire.com/file/p1y7cxjn5yrm1l3/VID-20230407-WA0188.mp4/file?dkey=yxt08zenag7&r=70",
+"https://www.mediafire.com/file/y3y96s0pa4xv3wv/abg_SmA_viral.mp4/file",
+"https://www.mediafire.com/file/adn9g8hhgovcb7b/9e7373836292+%281%29.mp4/file",
+"https://www.mediafire.com/file/vovcok123prvyg0/VID-20230620-WA0003.mp4/file",
+"https://www.mediafire.com/file/el4uj5t3r32s4cd/Nana_imut_17.mp4/file",
+"https://www.mediafire.com/file/mqqcv59hyodalgc/Kesakitan.mp4/file",
+"https://www.mediafire.com/file/1bg4za5nb5d0e8y/Esempeh_mabar_sama_ayah_sampe_mewek_.mp4/file",
+"https://www.mediafire.com/file/0f8e1ahfuzuibhq/Kesempatan.mp4/file",
+"https://www.mediafire.com/file/w8zzfj1mzutoh3m/x3DQJdR6.mp4.3gpp/file",
+"https://www.mediafire.com/file/fd5exlsxos7k4pi/VID-20240731-WA0054.mp4/file",
+"https://www.mediafire.com/file/gl4sa7cdvzljegn/VID-20240731-WA0053.mp4/file",
+"https://www.mediafire.com/file/o2m61um41lbz7s9/Kepunyaan_kakak_ngampang_lembab.mp4/file",
+"https://www.mediafire.com/file/qk7zu27hcflyn82/kainaoa_minta_jatah_sama_ayang.mp4/file",
+"https://www.mediafire.com/file/fqxga0cujitbwdf/Ngewe_teman_sendiri_saat_mabuk.mp4/file",
+"https://www.mediafire.com/file/d47qa82gpddvya5/kk_kacamata_omk.mp4/file",
+"https://www.mediafire.com/file/ihl8frr7muguwl5/bersama_ayang.mp4/file",
+"https://www.mediafire.com/file/v7zusuhrw1w5rkb/manja_pengennya_ayang_yg_om3k.mp4/file",
+"https://www.mediafire.com/file/0jje9g4v1izfcxe/kebaya+putih.MP4/file",
+"https://www.mediafire.com/file/2pcye34ij8zcki5/B4iCexpc.mp4/file",
+"https://www.mediafire.com/file/pt9cpc2nwqganhq/VID-20240614-WA0036.mp4/file",
+"https://www.mediafire.com/file/w8zzfj1mzutoh3m/x3DQJdR6.mp4.3gpp/file",
+"https://www.mediafire.com/file/fuzdjwu8l5zjnwa/sampek_nangis.mp4/file",
+"https://www.mediafire.com/file/e4rqsm5ouz92uul/crt_di_muka.mp4/file",
+"https://www.mediafire.com/file/05york0sjwxijgu/lagi_tidur_malah_di_ganggu.mp4/file"
+]
+
+let Reza = viral[Math.floor(Math.random() * viral.length)];
+	let response = await fetch(`https://api.agatz.xyz/api/mediafire?url=${Reza}`);
+        if (!response.ok) throw new Error(`Error fetching data: ${response.statusText}`);
+        
+        let data = await response.json();
+
+        // Mengakses data dengan indeks [0]
+        let fileData = data.data[0];
+      const result = fileData.link
+      res.status(200).json({
+        status: true,
+        result,
+      });
+    } catch (error) {
+      console.error("Error in /nsfw/nekopoi:", error); // Tambahkan logging untuk kesalahan
+      res.status(500).json({ status: false, error: error.message });
+    }
+  });
+};
