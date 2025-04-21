b14370714e8ded4d9090ee8d2ae7017b802cd61c
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2025-04-21 22:10:53 +0700

    Create mlstalk.js

---
+++ src/api/stalker/mlstalk.js
@@ -0,0 +1,25 @@
+let fetch = require('node-fetch');
+module.exports = (app) => {
+  app.get("/stalker/mlstalk", async (req, res) => {
+    try {
+      const { id, zona} = req.query; // Menggunakan q dari query
+      if (!id || !zona) {
+        return res.status(400).json({ status: false, error: "Query is required" });
+      }
+
+      // Menggunakan q sebagai parameter untuk API
+      const response = await fetch(`https://api.lolhuman.xyz/api/mobilelegend/${id}/${zona}?apikey=${process.env.lolhuman}`);
+      const hasil = await response.json();
+
+     
+      const result = hasil.result; // Menyimpan hasil dari API
+      res.status(200).json({
+        status: true,
+        result,
+      });
+    } catch (error) {
+      console.error("Error in /stalker/mlstalk:", error); // Tambahkan logging untuk kesalahan
+      res.status(500).json({ status: false, error: error.message });
+    }
+  });
+};
