c296373a1e686c450e9ac6e049d0c15c50f08e11
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2025-09-18 19:12:00 +0700

    Update mediafire.js

--- src/api/downloader/mediafire.js
+++ src/api/downloader/mediafire.js
@@ -3,20 +3,19 @@ const { fetch } = require("undici");
 
 module.exports = (app) => {
   
-
   app.get("/downloader/mediafire", async (req, res) => {
     try {
       const { url } = req.query;
-     let response = await fetch(`https://izumiiiiiiii.dpdns.org/downloader/mediafire?url=${url}`);
-        let data = await response.json();
+      let response = await fetch(`https://izumiiiiiiii.dpdns.org/downloader/mediafire?url=${url}`);
+      let data = await response.json();
 
-        // Mengakses data
-        if (!data.status || !data.result) throw new Error("File data not found");
+      // Mengakses data
+      if (!data.status || !data.result) throw new Error("File data not found");
 
-        let fileData = data.result;
+      let fileData = data.result;
       res.status(200).json({
         status: true,
-        result,
+        result: fileData, // Menggunakan fileData di sini
       });
     } catch (error) {
       res.status(500).json({ status: false, error: error.message });
