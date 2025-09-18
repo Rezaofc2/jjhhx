a481630349ce557b834d28a47615aae685aa4aa2
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2025-09-19 00:03:54 +0700

    Update toanime.js

--- src/api/ai/toanime.js
+++ src/api/ai/toanime.js
@@ -1,8 +1,6 @@
 const axios = require("axios");
 
 module.exports = (app) => {
-  
-
   app.get("/ai/toanime", async (req, res) => {
     try {
       const { imageUrl, style } = req.query;
@@ -11,13 +9,19 @@ module.exports = (app) => {
         return res.status(400).json({ status: false, error: "imageUrl is required" });
       }
 
-      const imageBuffer = `https://api.ryzumi.vip/api/ai/toanime?url=${imageUrl}&style=${style}`
+      // Mengambil gambar dari URL yang diberikan
+      const response = await axios.get(`https://api.ryzumi.vip/api/ai/toanime?url=${imageUrl}&style=${style}`, {
+        responseType: 'arraybuffer' // Mengatur responseType untuk mendapatkan buffer
+      });
 
+      // Mengatur header respons
       res.writeHead(200, {
         "Content-Type": "image/png",
-        "Content-Length": imageBuffer.length,
+        "Content-Length": response.data.length,
       });
-      res.end(imageBuffer);
+
+      // Mengirimkan gambar sebagai respons
+      res.end(response.data);
     } catch (error) {
       res.status(500).json({ status: false, error: error.message });
     }
