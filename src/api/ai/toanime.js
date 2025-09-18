33bff92f761fa0e344bf626394bbeb45df902a51
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2025-09-19 00:00:52 +0700

    Update toanime.js

--- src/api/ai/toanime.js
+++ src/api/ai/toanime.js
@@ -1,10 +1,7 @@
 const axios = require("axios");
 
 module.exports = (app) => {
-  async function toanime(imageUrl, style) {
-    const { data } = await axios.get(`https://api.ryzumi.vip/api/ai/toanime?url=${imageUrl}&style=${style}`);
-    return data; // Kembalikan data biner gambar
-  }
+  
 
   app.get("/ai/toanime", async (req, res) => {
     try {
@@ -14,7 +11,7 @@ module.exports = (app) => {
         return res.status(400).json({ status: false, error: "imageUrl is required" });
       }
 
-      const imageBuffer = await toanime(imageUrl, style); // Ambil data gambar langsung
+      const imageBuffer = `https://api.ryzumi.vip/api/ai/toanime?url=${imageUrl}&style=${style}`
 
       res.writeHead(200, {
         "Content-Type": "image/png",
