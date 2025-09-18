0872823fbeb9801cdfe85f72b3b32ba0733abdc7
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2025-09-18 22:13:29 +0700

    Update toanime.js

--- src/api/ai/toanime.js
+++ src/api/ai/toanime.js
@@ -1,33 +1,31 @@
-const axios = require("axios");
+const axios = require("axios")
 
 module.exports = (app) => {
-  async function toanime(imageUrl, style) {
-    const { data } = await axios.get(`https://api.ryzumi.vip/api/ai/toanime?url=${imageUrl}&style=${style}`);
-    return data; // Kembalikan seluruh data
-  }
+    app.get("/ai/tonime", async (req, res) => {
+        try {
+            const {
+                imageUrl, style
+            } = req.query
 
-  app.get("/ai/toanime", async (req, res) => {
-    try {
-      const { imageUrl, style } = req.query; // Perbaiki penamaan variabel di sini
+            if (!imageUrl) {
+                return res.status(400).json({
+                    status: false,
+                    error: "imageUrl is required"
+                })
+            }
 
-      if (!imageUrl) {
-        return res.status(400).json({ status: false, error: "imageUrl is required" });
-      }
-
-      const result = await toanime(imageUrl, style);
-      
-
-      // Mengambil gambar dari URL yang diberikan
-      const imageResponse = await axios.get(result, { responseType: 'arraybuffer' });
-      const imageBuffer = Buffer.from(imageResponse.data, "binary");
-
-      res.writeHead(200, {
-        "Content-Type": "image/png",
-        "Content-Length": imageBuffer.length,
-      });
-      res.end(imageBuffer);
-    } catch (error) {
-      res.status(500).json({ status: false, error: error.message });
-    }
-  });
+            const {
+                data
+            } = await axios.get(`https://api.ryzumi.vip/api/ai/toanime?url=${imageUrl}&style=${style}`)
+            res.status(200).json({
+                status: true,
+                result: data.result
+            })
+        } catch (error) {
+            res.status(500).json({
+                status: false,
+                error: error.message
+            })
+        }
+    })
 }
