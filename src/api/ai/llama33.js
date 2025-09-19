f8a7cce03d9d979e746e9a988b3c77c8da77bb1b
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2025-09-19 13:50:42 +0700

    Create llama33.js

---
+++ src/api/ai/llama33.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+
+module.exports = (app) => {
+  async function metaai(prompt,text) {
+    try {
+      // Menggunakan template literal yang benar dengan backticks (``)
+      const response = await axios.get(`https://api.siputzx.my.id/api/ai/llama33?prompt=${prompt}&text=${text}`);
+      return response.data.data; // Pastikan untuk mengakses data yang benar
+    } catch (error) {
+      console.error("Error fetching content:", error);
+      throw error;
+    }
+  }
+
+  app.get("/ai/metai", async (req, res) => {
+    const { prompt, text } = req.query; // Ambil parameter dari query string
+    if (!text) {
+      return res.status(400).json({ status: false, error: "Parameter 'text' is required." });
+    }
+    
+    try {
+      const result = await metaai(prompt, text); // Kirim parameter ke fungsi gemini
+      res.status(200).json({
+        status: true,
+        result,
+      });
+    } catch (error) {
+      res.status(500).json({ status: false, error: error.message });
+    }
+  });
+};
