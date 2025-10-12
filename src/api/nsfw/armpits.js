af1e1cb2d356abb636a47abb3822bd5ec3b99af0
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2025-10-13 05:42:31 +0700

    Update armpits.js

--- src/api/nsfw/armpits.js
+++ src/api/nsfw/armpits.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
     try {
       // Memanggil API untuk mendapatkan gambar NSFW Loli
       const imageResponse = await axios.get(
-        "https://api.lolhuman.xyz/api/random/nsfw/armpits?apikey=" + process.env.lolhuman,
+        "https://api.lolhuman.xyz/api/random/nsfw/armpits?apikey=" + global.lolkey,
         { responseType: "arraybuffer" },
       )
 
