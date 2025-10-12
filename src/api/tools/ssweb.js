8563919103776cd5736491447a39404892a59004
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2025-10-12 21:48:21 +0700

    Update ssweb.js

--- src/api/tools/ssweb.js
+++ src/api/tools/ssweb.js
@@ -5,28 +5,27 @@ module.exports = (app) => {
     try {
       const { url } = req.query;
 
+      // Memastikan URL diisi
       if (!url) {
         return res.status(400).json({ status: false, error: "Url is required" });
       }
 
       // Mengambil screenshot dari API
-      const response = await axios.get(`https://apidl.asepharyana.tech/api/tool/ssweb?url=${url}&mode=desktop`);
+   
 
-      // Mendapatkan URL file gambar dari respons
-      const { fileUrl } = response.data;
+    const imageResponse = await axios.get(
+       `https://apidl.asepharyana.tech/api/tool/ssweb?url=${encodeURIComponent(url)}&mode=desktop`,
+        { responseType: "arraybuffer" },
+      )
 
-      if (!fileUrl) {
-        return res.status(500).json({ status: false, error: "Failed to retrieve image URL" });
-      }
-
-      // Mengambil gambar dari URL yang diberikan
-      const imageResponse = await axios.get(fileUrl, { responseType: "arraybuffer" });
-
+      // Mengatur header response
       res.writeHead(200, {
         "Content-Type": "image/png",
         "Content-Length": imageResponse.data.length,
-      });
-      res.end(imageResponse.data);
+      })
+
+      // Mengirimkan data gambar
+      res.end(imageResponse.data)
     } catch (error) {
       res.status(500).json({ status: false, error: error.message });
     }
