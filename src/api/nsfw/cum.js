2f68d6f1e2b24d916424c58836a79d265283342d
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2025-10-13 05:43:51 +0700

    Update cum.js

--- src/api/nsfw/cum.js
+++ src/api/nsfw/cum.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
     try {
       // Memanggil API untuk mendapatkan gambar NSFW Loli
       const imageResponse = await axios.get(
-        "https://api.lolhuman.xyz/api/random2/cum_jpg?apikey=" + process.env.lolhuman,
+        "https://api.lolhuman.xyz/api/random2/cum_jpg?apikey=" + global.lolkey,
         { responseType: "arraybuffer" },
       )
 
