ed43f637f8028ffde1108408f60ed2aa6a79fb58
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2025-10-12 21:31:17 +0700

    Update removebg.js

--- src/api/tools/removebg.js
+++ src/api/tools/removebg.js
@@ -1,32 +1,35 @@
-const axios = require("axios")
+const axios = require("axios");
 
 module.exports = (app) => {
   app.get("/tools/removebg", async (req, res) => {
-    
-     try {
-            const {
-                imageUrl
-            } = req.query
+    try {
+      const { imageUrl } = req.query;
 
-            if (!imageUrl) {
-                return res.status(400).json({
-                    status: false,
-                    error: "imageUrl is required"
-                })
-            }
+      if (!imageUrl) {
+        return res.status(400).json({
+          status: false,
+          error: "imageUrl is required",
+        });
+      }
 
-            const {
-                data
-            } = await axios.get("https://rynekoo-api.hf.space/tools/pxpic/removebg?imageUrl=" + imageUrl)
-            res.status(200).json({
-                status: true,
-                result: data.result
-            })
-        } catch (error) {
-            res.status(500).json({
-                status: false,
-                error: error.message
-            })
-}
-})
-          }
+      const { data } = await axios.get("https://api.deline.my.id/tools/removebg?url=" + imageUrl);
+
+      // Mengambil hanya height, width, dan cutoutUrl dari data.result
+      const result = {
+        height: data.result.height,
+        width: data.result.width,
+        cutoutUrl: data.result.cutoutUrl,
+      };
+
+      res.status(200).json({
+        status: true,
+        result: result,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: false,
+        error: error.message,
+      });
+    }
+  });
+};
