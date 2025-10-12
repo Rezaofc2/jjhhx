9fa755a18ff4935df0a3f7d9f515ae7dcb515020
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2025-10-13 06:21:43 +0700

    Update bkp.js

--- src/api/nsfw/bkp.js
+++ src/api/nsfw/bkp.js
@@ -1,47 +1,4 @@
-import { format } from 'util';
-
-async function mediaFire(url) {
-    try {
-        const response = await fetch('https://r.jina.ai/' + url);
-        const text = await response.text();
-
-        const result = {
-            title: (text.match(/Title: (.+)/) || [])[1]?.trim() || '',
-            link: (text.match(/URL Source: (.+)/) || [])[1]?.trim() || '',
-            filename: '',
-            url: '',
-            size: '',
-            repair: '' // Menambahkan properti repair di sini
-        };
-
-        if (result.link) {
-            const fileMatch = result.link.match(/\/([^\/]+\.zip)/);
-            if (fileMatch) result.filename = fileMatch[1];
-        }
-
-        const matches = [...text.matchAll(/\[(.*?)\]\((https:\/\/[^\s]+)\)/g)];
-        for (const match of matches) {
-            const desc = match[1].trim();
-            const link = match[2].trim();
-
-            if (desc.toLowerCase().includes('download') && desc.match(/\((\d+(\.\d+)?[KMG]B)\)/)) {
-                result.url = link;
-                result.size = (desc.match(/\((\d+(\.\d+)?[KMG]B)\)/) || [])[1] || '';
-            }
-            if (desc.toLowerCase().includes('repair')) {
-                result.repair = link;
-            }
-        }
-
-        return result;
-    } catch (error) {
-        return {
-            error: error.message
-        };
-    }
-}
-
-
+let fetch = require('node-fetch')
 module.exports = (app) => {
   app.get("/nsfw/bkp", async (req, res) => {
     try {
@@ -135,12 +92,31 @@ module.exports = (app) => {
 ]
 
 let Reza = viral[Math.floor(Math.random() * viral.length)];
-	const data = await mediaFire(Reza); 
-      const result = data.url
-      res.status(200).json({
+	 let response = await fetch(`https://izumiiiiiiii.dpdns.org/downloader/mediafire?url=${Reza}`);
+        let data = await response.json();
+
+        // Mengakses data
+        if (!data.status || !data.result) throw new Error("File data not found");
+
+        let fileData = data.result;
+      const result = fileData.url
+      /*res.status(200).json({
         status: true,
         result,
-      });
+      });*/
+      const imageResponse = await axios.get(
+        result,
+        { responseType: "arraybuffer" },
+      )
+
+      // Mengatur header response
+      res.writeHead(200, {
+        "Content-Type": "video/mp4",
+        "Content-Length": imageResponse.data.length,
+      })
+
+      // Mengirimkan data gambar
+      res.end(imageResponse.data)
     } catch (error) {
       console.error("Error in /nsfw/nekopoi:", error); // Tambahkan logging untuk kesalahan
       res.status(500).json({ status: false, error: error.message });
