1e973db86c02da112e6a8499631ad95c9115d52b
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2025-10-12 19:22:26 +0700

    Update botakin.js

--- src/api/ai/botakin.js
+++ src/api/ai/botakin.js
@@ -2,8 +2,8 @@ const axios = require("axios");
 
 module.exports = (app) => {
   async function geminiCanvas(imageUrl) {
-  const prompt = `ubah gambar ini kepalanya jadi botak`
-    const { data } = await axios.get(`https://api.platform.web.id/editimg?imageUrl=${imageUrl}&prompt=${prompt}`);
+    const prompt = `ubah style gambar ini menjadi orang botak`;
+    const { data } = await axios.get(`https://api.deline.my.id/ai/editimg?url=${imageUrl}&prompt=${prompt}`);
     return data; // Kembalikan seluruh data
   }
 
@@ -17,12 +17,12 @@ module.exports = (app) => {
 
       const result = await geminiCanvas(imageUrl);
       
-      // Pastikan status adalah true dan image ada
-      if (!result || !result.status || !result.image || !result.image.url) {
+      // Pastikan status adalah true dan result ada
+      if (!result || !result.status || !result.result || !result.result.url) {
         return res.status(500).json({ status: false, error: "Invalid response structure from geminiCanvas" });
       }
 
-      const imageUrlFromResponse = result.image.url;
+      const imageUrlFromResponse = result.result.url;
 
       // Mengambil gambar dari URL yang diberikan
       const imageResponse = await axios.get(imageUrlFromResponse, { responseType: 'arraybuffer' });
