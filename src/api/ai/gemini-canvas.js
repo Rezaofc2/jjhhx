4f6db7cf85c8bf24f077c16f40f97e94986db66a
RezaOffc <106691452+Rezaofc2@users.noreply.github.com>
2025-10-20 19:13:37 +0700

    Update gemini-canvas.js

--- src/api/ai/gemini-canvas.js
+++ src/api/ai/gemini-canvas.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 
 module.exports = (app) => {
   
+
   app.get("/ai/gemini-canvas", async (req, res) => {
     try {
       const { text, imageUrl } = req.query;
@@ -14,27 +15,18 @@ module.exports = (app) => {
         return res.status(400).json({ status: false, error: "text is required" });
       }
 
-      // Mengubah URL sesuai dengan permintaan
-      const apiUrl = `https://rynekoo-api.hf.space/ai/qwen/image-edit?prompt=${encodeURIComponent(text)}&imageUrl=${encodeURIComponent(imageUrl)}`;
+     const imageResponse = await axios.get(`https://apidl.asepharyana.tech/api/ai/image/gemini?text=${encodeURIComponent(text)}&url=${encodeURIComponent(imageUrl)}`,
+        { responseType: "arraybuffer" },
+      )
 
-      const imageResponse = await axios.get(apiUrl, { responseType: "arraybuffer" });
+      // Mengatur header response
+      res.writeHead(200, {
+        "Content-Type": "image/png",
+        "Content-Length": imageResponse.data.length,
+      })
 
-      // Memeriksa apakah permintaan berhasil
-      
-        const imageUrlResult = imageResponse.data.result;
-
-        // Mengambil gambar dari URL hasil
-        const finalImageResponse = await axios.get(imageUrlResult, { responseType: "arraybuffer" });
-
-        // Mengatur header response untuk gambar
-        res.writeHead(200, {
-          "Content-Type": "image/webp", // Atur sesuai dengan format gambar yang dihasilkan
-          "Content-Length": finalImageResponse.data.length,
-        });
-
-        // Mengirimkan data gambar
-        res.end(finalImageResponse.data);
-      
+      // Mengirimkan data gambar
+      res.end(imageResponse.data)
     } catch (error) {
       res.status(500).json({ status: false, error: error.message });
     }
